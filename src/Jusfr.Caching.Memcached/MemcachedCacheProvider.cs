e5dd703c8a1ef4e8adc7a1652029de2ba4c09fc7
Jusfr <jusfr.v@gmail.com>
2015-04-02 11:34:28 +0800

    实现 Memcached 的滑动过期

--- src/Jusfr.Caching.Memcached/MemcachedCacheProvider.cs
+++ src/Jusfr.Caching.Memcached/MemcachedCacheProvider.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
+using System.Web.Caching;
 
 namespace Jusfr.Caching.Memcached {
     public class MemcachedCacheProvider : CacheProvider, IHttpRuntimeCacheProvider, IRegion {
@@ -29,21 +30,31 @@ namespace Jusfr.Caching.Memcached {
             return Region == null ? key : String.Concat(Region, "_", key);
         }
 
-        private Boolean InnerTryGet(string key, out object entry) {
-            return _client.TryGet(key, out entry);
+        // Will not last expire time
+        public Boolean TryGetObject(string key, out object entry) {
+            return _client.TryGet(BuildCacheKey(key), out entry);
         }
 
-        public override bool TryGet<T>(string key, out T entry) {
-            String cacheKey = BuildCacheKey(key);
+        public override bool TryGet<T>(string key, out T entry) {            
             Object cacheEntry;
-            Boolean exist = InnerTryGet(cacheKey, out cacheEntry);
+            Boolean exist = TryGetObject(key, out cacheEntry);
             if (exist) {
                 if (cacheEntry != null) {
-                    if (!(cacheEntry is T)) {
+                    if (cacheEntry is ExpirationWraper<T>) {
+                        var cacheWraper = (ExpirationWraper<T>)cacheEntry;
+                        if (cacheWraper.AbsoluteExpiration == Cache.NoAbsoluteExpiration) {
+                            //表示滑动过期缓存项，重新存储
+                            Overwrite(key, cacheWraper.Value, cacheWraper.SlidingExpiration);
+                        }
+                        entry = cacheWraper.Value;
+                    }
+                    else if (!(cacheEntry is T)) {
                         throw new InvalidOperationException(String.Format("缓存项`[{0}]`类型错误, {1} or {2} ?",
                             key, cacheEntry.GetType().FullName, typeof(T).FullName));
                     }
-                    entry = (T)cacheEntry;
+                    else {
+                        entry = (T)cacheEntry;
+                    }
                 }
                 else {
                     entry = (T)((Object)null);
@@ -81,7 +92,8 @@ namespace Jusfr.Caching.Memcached {
 
         //slidingExpiration 时间内无访问则过期
         public void Overwrite<T>(String key, T value, TimeSpan slidingExpiration) {
-            _client.Store(StoreMode.Set, BuildCacheKey(key), value, slidingExpiration);
+            var cacheWraper = new ExpirationWraper<T>(value, slidingExpiration);
+            _client.Store(StoreMode.Set, BuildCacheKey(key), cacheWraper, slidingExpiration);
         }
 
         //absoluteExpiration 时过期
@@ -91,6 +103,48 @@ namespace Jusfr.Caching.Memcached {
 
         public override void Expire(String key) {
             _client.Remove(BuildCacheKey(key));  // Could check result
+        }
+
+        [Serializable]
+        public class ExpirationWraper<T> {
+            public T Value { get; private set; }
+            public DateTime AbsoluteExpiration { get; private set; }
+            public TimeSpan SlidingExpiration { get; private set; }
+
+            public ExpirationWraper(T value, DateTime absoluteExpiration)
+                : this(value, absoluteExpiration, Cache.NoSlidingExpiration) {
+            }
+
+            public ExpirationWraper(T value, TimeSpan slidingExpiration)
+                : this(value, Cache.NoAbsoluteExpiration, slidingExpiration) {
+            }
+
+            private ExpirationWraper(T value, DateTime absoluteExpiration, TimeSpan slidingExpiration) {
+                Value = value;
+                AbsoluteExpiration = absoluteExpiration;
+                SlidingExpiration = slidingExpiration;
+            }
+        }
+
+        public struct NullableEntry<T> {
+            private T entry;
+            private Boolean isNull;
+
+            public NullableEntry(T value) {
+                entry = value;
+                isNull = (value == null);
+            }
+
+            public static implicit operator NullableEntry<T>(T value) {
+                return new NullableEntry<T>(value);
+            }
+
+            public static implicit operator T(NullableEntry<T> value) {
+                if (value.isNull) {
+                    return (T)((Object)null);
+                }
+                return value.entry;
+            }
         }
     }
 }
