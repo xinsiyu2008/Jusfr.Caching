9909dcfc429e23e597c768b5e547fa5552633bf0
Jusfr <jusfr.v@gmail.com>
2015-04-17 16:56:09 +0800

    改进build脚本

--- build.bat
+++
@@ -1,8 +0,0 @@
-msbuild src\Jusfr.Caching\Jusfr.Caching.csproj
-robocopy src\Jusfr.Caching\bin\Debug debug\Jusfr.Caching /mir
-
-msbuild src\Jusfr.Caching.Memcached\Jusfr.Caching.Memcached.csproj
-robocopy src\Jusfr.Caching.Memcached\bin\Debug debug\Jusfr.Caching.Memcached /mir
-
-msbuild src\Jusfr.Caching.Mongodb
-robocopy src\Jusfr.Caching.Mongodb\bin\Debug debug\Jusfr.Caching.Mongodb /mir
---
+++ build_Jusfr_Caching.bat
@@ -0,0 +1,18 @@
+@setlocal 
+@set local=%~dp0
+@pushd %WINDIR%\Microsoft.NET\Framework\v4.0.30319\
+@goto build
+
+
+:build
+msbuild "%local%src\Jusfr.Caching.sln" /t:Rebuild /P:Configuration=Release
+@goto copy
+
+:copy
+robocopy "%local%src\Jusfr.Caching\bin\Release" %local%release /e
+robocopy "%local%src\Jusfr.Caching.Memcached\bin\Release" %local%release /e
+@goto end
+
+:end
+@pushd %local%
+@pause
