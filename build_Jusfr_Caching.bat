b21fac6417eade4bb81be658118c20e5fb0df868
Jusfr <jusfr.v@gmail.com>
2015-10-09 16:22:25 +0800

    修改 bat 脚本

--- build_Jusfr_Caching.bat
+++ build_Jusfr_Caching.bat
@@ -9,7 +9,7 @@ msbuild "%local%src\Jusfr.Caching.sln" /t:Rebuild /P:Configuration=Release
 @goto copy
 
 :copy
-robocopy "%local%src\Jusfr.Caching\bin\Release" %local%release /e
+robocopy "%local%src\Jusfr.Caching\bin\Release" %local%release /mir
 robocopy "%local%src\Jusfr.Caching.Memcached\bin\Release" %local%release /e
 robocopy "%local%src\Jusfr.Caching.Redis\bin\Release" %local%release /e
 @goto end
